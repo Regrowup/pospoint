2b483e5c2bcf02443c469bdbd486666c3e2e0e07
Abdul Fakhri <abf.ca@hotmail.com>
2022-01-26 15:01:34 -0800

    update

--- login.php
+++ login.php
@@ -155,12 +155,100 @@ if(isset($_POST['login'])){
 
 ?>
 <?php include ("header.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: white;
+}
 
-    <form class="modal-content" action=""  method="POST">
-    <div class="container">
-    <h3>Admin Sign In</h3>
-     
-     <input name="email" type="text" value="" placeholder="Email..." required />
+* {
+  box-sizing: border-box;
+}
+
+/* Add padding to containers */
+.container {
+  padding: 16px;
+  background-color: white;
+  width: 60%;
+  margin-left: 20%;
+  margin-right: 20%;
+  margin-top: 5%;
+  border: 1px solid #f1f1f1;
+  border-radius: 10px;
+  box-shadow: 3px 3px 5px #f1f1f1;
+}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Overwrite default styles of hr */
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+/* Set a style for the submit button */
+.loginbtn {
+  background-color: #000;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 50%;
+  opacity: 0.9;
+}
+.cancelbtn {
+  background-color: #f44336;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 50%;
+  opacity: 0.9;
+}
+
+.registerbtn:hover {
+  opacity: 1;
+}
+
+/* Add a blue text color to links */
+a {
+  color: dodgerblue;
+}
+
+/* Set a grey background color and center the text of the "sign in" section */
+.signin {
+  background-color: #f1f1f1;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+<form action="" method="POST">
+  <div class="container">
+  <h3>Admin Sign In</h3>
+    <hr>
+
+    <input name="email" type="text" value="" placeholder="Email..." required />
 
      <input type="password" placeholder="Password" name="password" required>
      <select class="form-control" name="role">
@@ -170,16 +258,17 @@ if(isset($_POST['login'])){
      </select>
      
      <div class="clearfix">
+     <button type="" class="cancelbtn"> Cancel</button>
+       <button type="submit" name="login" class="loginbtn">Sign In</button>
       
-       <button type="submit" name="login" class="signupbtn">Sign In</button>
-       <button type="" class="cancelbtn"> Cancel</button>
        <input type="checkbox"> Remember me<br></br>
            
            Forgot <a href="#"> password? </a>
-     </div>
-    </div>
-  </form>
+  </div>
+  </div>
   
-</body>
+ 
+</form>
 
+</body>
 </html>
