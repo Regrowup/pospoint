bf0b8031c0ba8e79b4ff08df64d43d36b139803c
Abdul Fakhri <abf.ca@hotmail.com>
2022-01-26 13:47:29 -0800

    update

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'root');
-define('DB_NAME', 'pospoint');
+define('DB_USER','u559678163_pospointdbu');
+define('DB_PASS' ,'!@#123qweasdZXC');
+define('DB_NAME', 'u559678163_pospointdb');
 $conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
