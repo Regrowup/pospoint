3691053729ef2eaca7639127f0881171bbc4f115
Abdul Fakhri <abf.ca@hotmail.com>
2022-01-27 00:50:01 -0800

    update

--- admin/nav.php
+++ admin/nav.php
@@ -191,7 +191,7 @@
    
       
         <!-- Top container -->
-        <div class="w3-bar w3-top w3-red w3-large" style="z-index:4">
+        <div class="w3-bar w3-top w3-black w3-large" style="z-index:4">
             <button class="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onclick="w3_open();"><i class="fa fa-bars"></i>  Menu</button>
        
       
